145dcc51f754c7f6718971af43951a9315ad02ec
LebowskiSama <anushvenkat101@gmail.com>
2020-05-22 03:29:37 +0530

    'setup-morse-conversion'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
-    <h1>Hello</h1>
-    <v-text-field style="width: 50%; margin-left: 25%" placeholder="Morse me..." v-model="message"></v-text-field>
+    <h1>Morsel</h1>
+    <v-text-field color="deep-purple accent-3" style="width: 75%; align-self: center;" placeholder="Morse me..." v-model="message" v-on:input="parseMorse"></v-text-field>
+    <h1 id="morseData">{{ morse }}</h1>
   </v-app>
 </template>
 
@@ -10,7 +11,11 @@ export default {
   name: 'App',
 
   data: () => ({
+
     message: "",
+
+    morse: null,
+
     morseData: {
         'A':'.-', 'B':'-...', 
                     'C':'-.-.', 'D':'-..', 'E':'.', 
@@ -27,11 +32,44 @@ export default {
                     '0':'-----', ', ':'--..--', '.':'.-.-.-', 
                     '?':'..--..', '/':'-..-.', '-':'-....-', 
                     '(':'-.--.', ')':'-.--.-'
-    }
+    },
   }),
+
+  methods: {
+    parseMorse: function() {
+      var splitArray = this.message.split('')
+      var morse = []
+      var i
+
+      for (i in splitArray){
+        morse[i] = this.morseData[splitArray[i].toUpperCase()]
+      }
+
+      this.morse = morse.join(' ')
+    }
+  }
+
 };
 </script>
 
 <style scoped>
+
+h1{
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 100px;
+  animation: fadeIn ease-in 1s;
+}
+
+
+
+@keyframes fadeIn{
+  from{
+    opacity: 0;
+  }
+  to{
+    opacity: 1;
+  }
+}
 
 </style>
