b8d052a860718a01314c4e6e93e084c100e86b92
LebowskiSama <anushvenkat101@gmail.com>
2020-05-22 17:37:50 +0530

    added-icons-and-slight-styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <v-app>
-    <h1>Morsel</h1>
+
+    <span id="pageIcons">
+      <v-btn width=0 class="icons" id="info-icon" dark>
+        <v-icon>info</v-icon>
+      </v-btn>
+      <v-btn class="icons" id="github-icon" dark>
+        <v-icon >mdi-github</v-icon>
+      </v-btn>
+    </span>
+
+      <p id="pageHeader">
+        <span>Morsel</span>
+      </p>
+
     <v-container id="vContainer">
       <v-text-field color="deep-purple accent-3" placeholder="Morse me..." v-model="message" v-on:input="parseMorse"></v-text-field>
     </v-container>
+
     <p>{{ morse }}</p>
+
   </v-app>
 </template>
 
@@ -45,34 +60,47 @@ export default {
 
       for (i in splitArray){
         morse[i] = this.morseData[splitArray[i].toUpperCase()]
-      }
+        }
 
+      for(i in morse){
+        if(typeof morse[i] == "undefined"){
+          morse[i] = '\xa0'
+        }
+      }
+      
       this.morse = morse.join('\xa0\xa0\xa0')
+
     }
   }
-
 };
 </script>
 
 <style scoped>
 
-h1{
-  text-align: center;
-  margin-top: 20px;
-  margin-bottom: 100px;
+#pageHeader{
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
   animation: fadeIn ease-in 0.5s;
 }
 
-#input-4{
-  margin-bottom: 0;
-  width: 50%; 
-  align-self: center;
-  animation: fadeIn ease-in 0.5s;
+.icons{
+  float: right;
+}
+
+#pageIcons{
+  margin-top: 5px;
+  margin-right: 5px;
+  margin-bottom: 50px;
+}
+
+#info-icon{
+  margin-left: 20px;
+  margin-right: 5px;
 }
 
 #vContainer{
   margin-bottom: 100px;
   width: 50%;
+  animation: fadeIn ease-in 0.5s;
 }
 
 p{
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify,{
+    iconfont: 'md'
+});
 
 export default new Vuetify({
 });
